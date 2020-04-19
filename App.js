83746b99cd83fa2f5f6f2cf3378f5052f8a82a41
mashad <a>
2020-04-19 06:15:18 +0500

    icon fixed

--- App.js
+++ App.js
@@ -19,84 +19,77 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
-const {width:WIDTH}= Dimensions.get('window')
+const {width: WIDTH} = Dimensions.get('window');
 
 const App = () => {
- 
   // state={
   //   hidePass:true,
   //   press:false
   // }
-  const [hidePass,sethidePass] = useState(true)
-  const [press,setpress] = useState(false)
-  
-  showPassbtn=()=>{
-    if (press == false){
-      sethidePass(false),
-      setpress(true)
+  const [hidePass, sethidePass] = useState(true);
+  const [press, setpress] = useState(false);
+
+  showPassbtn = () => {
+    if (press == false) {
+      sethidePass(false), setpress(true);
+    } else {
+      sethidePass(true), setpress(false);
     }
-    else {
-      sethidePass(true),
-      setpress(false)
-    }
-  }
+  };
 
   return (
     <View style={styles.scrollView}>
-    
       <View style={styles.LogoContainer}>
         <Text style={styles.LogoText}>LOGIN PAGE</Text>
       </View>
 
       <View style={styles.body}>
         <Icon
-          name= {"android-person-outline"}
+          name={'ios-person'}
           size={26}
           style={styles.Iconstyle}
           color={'rgba(0,0,0,0.3)'}
         />
         <TextInput
-        style={styles.Input}
-        placeholder={'username'}
-        placeholderTextColor={'rgba(255,255,0.7)'}
-        underlineColorAndroid='transparent'
+          style={styles.Input}
+          placeholder={'username'}
+          placeholderTextColor={'rgba(255,255,0.7)'}
+          underlineColorAndroid="transparent"
         />
       </View>
 
       <View style={styles.body}>
         <Icon
-          name= {'lock-outline'}
+          name={'md-lock'}
           size={26}
           color={'rgba(0,0,0,0.3)'}
           style={styles.Iconstyle}
         />
         <TextInput
-        style={styles.Input}
-        placeholder={'Password'}
-        secureTextEntry={hidePass}
-        placeholderTextColor={'rgba(255,255,0.7)'}
-        underlineColorAndroid='transparent'
+          style={styles.Input}
+          placeholder={'Password'}
+          secureTextEntry={hidePass}
+          placeholderTextColor={'rgba(255,255,0.7)'}
+          underlineColorAndroid="transparent"
         />
-      
-        <TouchableOpacity style={styles.eyebtn} onPress={()=>showPassbtn()} >
+
+        <TouchableOpacity style={styles.eyebtn} onPress={() => showPassbtn()}>
           <Icon
-            name={press == false ? 'ios-eye-outline':'ios-eye-off-outline'}
+            name={press == false ? 'ios-eye' : 'ios-eye-off'}
             size={26}
             color={'rgba(0,0,0,0.3)'}
           />
         </TouchableOpacity>
-      
       </View>
-      <View >
-      <TouchableOpacity style={styles.loginbtn} >
-        <Text style={styles.btnText}>Login</Text>
-      </TouchableOpacity>
-   
-      <TouchableOpacity style={styles.signupbtn}>
-        <Text style={styles.btnText}>Sign up</Text>
-      </TouchableOpacity>
-      </View>
+      <View>
+        <TouchableOpacity style={styles.loginbtn}>
+          <Text style={styles.btnText}>Login</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity style={styles.signupbtn}>
+          <Text style={styles.btnText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -104,64 +97,64 @@ const App = () => {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
-    flex:1,
-    justifyContent:"center",
-    alignItems:'center'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  body:{
-    marginTop:10
+  body: {
+    marginTop: 10,
   },
-  LogoText:{
-    fontSize:20,
-    fontWeight:'500',
-    marginBottom:10,
-    opacity:0.5
+  LogoText: {
+    fontSize: 20,
+    fontWeight: '500',
+    marginBottom: 10,
+    opacity: 0.5,
   },
-  LogoContainer:{
-    alignItems:"center",
+  LogoContainer: {
+    alignItems: 'center',
     // padding:10
-    marginBottom:50
+    marginBottom: 50,
   },
-  Input:{
-    width:WIDTH-90,
-    height:40,
-    borderRadius:45,
-    fontSize:18,
-    paddingLeft:45,
-    backgroundColor:'rgba(0,0,0,0.1)',
-    marginHorizontal:25,
+  Input: {
+    width: WIDTH - 90,
+    height: 40,
+    borderRadius: 45,
+    fontSize: 18,
+    paddingLeft: 45,
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    marginHorizontal: 25,
   },
-  Iconstyle:{
-    position: "absolute",
-    top:8,
-    left:37
+  Iconstyle: {
+    position: 'absolute',
+    top: 8,
+    left: 37,
   },
-  eyebtn:{
-    position:"absolute",
-    top:8,
-    right:37
+  eyebtn: {
+    position: 'absolute',
+    top: 8,
+    right: 37,
   },
-  loginbtn:{
-    width:WIDTH-180,
-    height:40,
-    borderRadius:45,
-    backgroundColor:"#008CBA",
-    justifyContent:"center",
-    marginTop:20
+  loginbtn: {
+    width: WIDTH - 180,
+    height: 40,
+    borderRadius: 45,
+    backgroundColor: '#008CBA',
+    justifyContent: 'center',
+    marginTop: 20,
   },
 
-  signupbtn:{
-    width:WIDTH-180,
-    height:40,
-    borderRadius:45,
-    backgroundColor:"#4CAF50",
-    justifyContent:"center",
-    marginTop:20
+  signupbtn: {
+    width: WIDTH - 180,
+    height: 40,
+    borderRadius: 45,
+    backgroundColor: '#4CAF50',
+    justifyContent: 'center',
+    marginTop: 20,
   },
-  btnText:{
-    textAlign:'center',
-    fontSize:16
-  }
+  btnText: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
 });
 
 export default App;
